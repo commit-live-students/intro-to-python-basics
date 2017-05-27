a0cfd13baf7cc9049e71a378535bb3c149aa9d5d
sangamangreg <sangam.angre@gmail.com>
2017-05-27 09:46:04 +0000

    Done

--- practice.py
+++ practice.py
@@ -3,3 +3,6 @@ s1 = "hello 12112345678902dasda dasd asdf asd fasfd sdaf sadf sdaf das fsadf asd
 s2 = "hello 12112345678902dasda dasd asdf asd fasfd sdaf sadf sdaf das fsadf asd d"
 
 print (id(s1), id(s2))
+
+
+print ("Hello")
