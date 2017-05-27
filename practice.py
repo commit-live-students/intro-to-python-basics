bb6d69b9396baffa59e8cb6cdc26ca5991020998
sangamangreg <sangam.angre@gmail.com>
2017-05-27 09:44:31 +0000

    Done

---
+++ practice.py
@@ -0,0 +1,5 @@
+s1 = "hello 12112345678902dasda dasd asdf asd fasfd sdaf sadf sdaf das fsadf asd d"
+
+s2 = "hello 12112345678902dasda dasd asdf asd fasfd sdaf sadf sdaf das fsadf asd d"
+
+print (id(s1), id(s2))
