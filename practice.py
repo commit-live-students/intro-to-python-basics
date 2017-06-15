c7612cc6376121efec20d782a4307fe27728e62f
sankbhatt <sankbhatt@users.noreply.github.com>
2017-06-15 16:04:46 +0000

    Done

---
+++ practice.py
@@ -0,0 +1,19 @@
+'''a = "hello world"
+print a'''
+
+n = "sanket bhatt"
+
+def myname(a):
+    """
+        here goes my function doc
+    """
+    n = "bhatt"
+    print "my name is "+a+n
+
+myname("asdasd")
+n = "sanket"
+myname(n)
+print myname.__doc__
+#print('"'*50)
+#print help(myname)
+print "संकेत"
